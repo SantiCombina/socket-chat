9bc9cdaa2606d40f230488236cd9bcd2f96611cd
SantiCombina <santicombina@gmail.com>
2024-04-12 17:14:51 -0300

    Translated error messages to english

--- app/src/schemas/home-schema.ts
+++ app/src/schemas/home-schema.ts
@@ -1,8 +1,8 @@
 import * as z from "zod";
 
 export const homeSchema = z.object({
-    user_name: z.string().min(1, "Debes acceder con un nombre").max(20, "Máximo 20 caracteres"),
-    room: z.string().min(1, "La sala es requerida"),
+    user_name: z.string().min(1, "You must log in with your name").max(20, "Maximum 20 characters"),
+    room: z.string().min(1, "The room is required"),
 });
 
 export type HomeValues = z.infer<typeof homeSchema>;
